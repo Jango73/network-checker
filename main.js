cb1ac302ad32b1ba52a67e0fa2007881a1600f06
Guillaume Darier <guillaumed00@gmail.com>
2025-05-25 12:20:53 +0200

    Fix dark mode

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const fs = require('fs').promises;
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1024,
+    height: 768,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
